8eb3e6fa92a3fc5ba41a9bf867d109f467a8bb6f
Erick Daniel Juarez Gil <erickdanieljuarezgil@gmail.com>
2018-03-28 12:03:53 -0600

    First Commit

---
+++ app/src/main/java/basicsolutionsoftware/com/wakemeupnow/SplashActivity.java
@@ -0,0 +1,13 @@
+package basicsolutionsoftware.com.wakemeupnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
